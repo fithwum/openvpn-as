2a65bf15549946d5da2baebf6fb57765e36be4e0
fithwum <fithwum@hotmail.com>
2023-01-31 07:29:37 -0800

    first commit

---
+++ Openvpn-as/files/Install_Script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2022 fithwum
+# All rights reserved
+
+# Variables.
+echo " "
+echo "INFO ! ."
+
+
+# Main install (Debian).
+
+sleep 1
+
+# Set permissions.
+chown 99:100 -R /OpenVPN-as
+chmod 777 -R /OpenVPN-as
+
+sleep 1
+
+# Run OpenVPN-as server.
+echo " "
+echo "INFO ! ."
+
+
+exit
