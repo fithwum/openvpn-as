74b88509b38e21c38fc6162877509ac37550efbd
fithwum <fithwum@hotmail.com>
2023-01-31 20:25:45 -0800

    testing

--- openvpn-as/files/Install_Script.sh
+++ openvpn-as/files/Install_Script.sh
@@ -8,9 +8,6 @@ echo "INFO ! ."
 
 
 # Main install (Debian).
-wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian bullseye main">/etc/apt/sources.list.d/openvpn-as-repo.list
-apt update && apt -y install openvpn-as
 
 sleep 1
 
